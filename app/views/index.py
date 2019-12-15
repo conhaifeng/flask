353702c21260e3a11beddfa13cf08abd1d6cc86c
haifeng.hu <haifeng.hu@wuage.com>
2019-12-15 11:57:45 +0800

    1.添加ok页，对接nginx

---
+++ app/views/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/9/18 19:21
+# @Author  : haifeng.hu
+
+from flask.blueprints import Blueprint
+
+ok_service = Blueprint("ok", __name__)
+
+@ok_service.route("/ok")
+def ok():
+    return "ok"
